dcdad4674a7ddaae36dc2212dfb9442e017a5b74
tasinet <tasinet@tasinet.gr>
2012-07-12 22:09:53 +0100

    .

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,12 @@
 class Feedback < ActiveRecord::Base
   attr_accessible :domain_id, :feedback_type, :email, :message, :fbid, :client_ip
   belongs_to :domain
+  
+  validates :domain_id, presence: true
+  validates :email, presence: true
+  validates :message, presence: true
+  validates :fbid, presence: true
+  validates :client_ip, presence: true
+  
+  default_scope order: 'feedbacks.created_at DESC'
 end
